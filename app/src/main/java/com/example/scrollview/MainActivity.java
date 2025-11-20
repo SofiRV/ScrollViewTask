8a0680ed43906629584c19ca72623c38f1da75e9
Sofi <srodriguezvanegas@cifpfbmoll.eu>
2025-11-20 19:53:54 +0100

    final changes added
    +button to add coments

--- app/src/main/java/com/example/scrollview/MainActivity.java
+++ app/src/main/java/com/example/scrollview/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.scrollview;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        EditText article = findViewById(R.id.article);
+        Button addCommentButton = findViewById(R.id.add_comment_button);
+        addCommentButton.setOnClickListener(v -> {
+            if(!article.isEnabled()){
+                article.setEnabled(true);
+                article.requestFocus();
+                addCommentButton.setText("Guardar");
+            }else {
+                article.setEnabled(false);
+                addCommentButton.setText("Añadir comentario");
+                String text = article.getText().toString();
+                getSharedPreferences("prefs", MODE_PRIVATE)
+                        .edit().putString("article", text).apply();
+            }
+        });
+        String text = getSharedPreferences("prefs", MODE_PRIVATE)
+                .getString("article", getString(R.string.article_text));
+        article.setText(text);
     }
 }
